0fb2b5fc4523c08194bfed86bd2b22c7f82d3a4f
Facundo Baez <facundo.baez@gmail.com>
2026-05-21 13:14:42 +0000

    Update siteProps with user information and enhance social icon animations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Facundo Baez",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "facundobaez@hotmail.com",
+  gitHub: "baezfacundo",
+  instagram: "baezfacundo",
+  linkedIn: "baezfacundo",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "baezfacundo",
+  youTube: "baezfacundo",
 };
 
 const primaryColor = "#4E567E";
